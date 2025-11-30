e15c6821298b3b5e1053a37beb3aa67e4d2c578c
M4chado <eduardo29066@gmail.com>
2025-11-30 16:32:19 -0300

    Config Inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1 style={{ color: 'var(--primary-color)' }}>OnMenu</h1>
+      <p>Seu cardápio digital começa aqui.</p>
+    </div>
+  );
+}
+
+export default App;
